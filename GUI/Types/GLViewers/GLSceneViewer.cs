099ca9aacf9498139f53b217c3e5b8db8e413c8d
Poggu <poggu@seznam.cz>
2026-08-09 15:48:59 +0200

    refactor: put teleport in new entity system

--- GUI/Types/GLViewers/GLSceneViewer.cs
+++ GUI/Types/GLViewers/GLSceneViewer.cs
@@ -226,10 +226,8 @@ namespace GUI.Types.GLViewers
                 Input.PhysicsWorld = Scene.PhysicsWorld;
             }
 
-            if (Scene.EntitySystem.Entities.Count > 0)
-            {
-                Input.EntitySystem = Scene.EntitySystem;
-            }
+            Input.EntitySystem = Scene.EntitySystem;
+            Scene.EntitySystem.SpawnPlayer(Input.PlayerMovement);
 
             SkyboxScene?.Initialize();
 
